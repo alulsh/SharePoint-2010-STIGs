5cea070bb2592bef3055e1c4137d295a9e7e2a04
Alexandra Ulsh <alexandra.ulsh@gmail.com>
2015-01-31 14:05:22 -0500

    Initial commit with folder structure and one script for IIS 7 site

---
+++ IIS Site/V-13702.ps1
@@ -0,0 +1,35 @@
+﻿<#
+Applies finding V-13702 from the IIS 7 Site STIG
+Title: The Content Location header must not contain proprietary IP addresses.
+Reference: http://www.stigviewer.com/stig/iis_7.0_web_site/2014-03-25/finding/V-13702
+#>
+
+function setAlternateHostName {
+
+    $fqdn = "$env:computername.$env:userdnsdomain"
+
+    $websites = Get-WebSite
+
+    foreach($website in $websites){
+
+        $siteName = $website.Name
+
+        $runtimeConfig = Get-WebConfiguration -Filter "/system.webServer/serverRuntime" -Location $siteName
+
+        if(!$runtimeConfig.alternateHostName){
+
+            Write-Host $siteName - Not STIG compliant - alternateHostName is blank
+
+            Set-WebConfigurationProperty -Filter "/system.webServer/serverRuntime" -Name alternateHostName -Value $fqdn
+
+        }
+
+        else {
+
+            Write-Host $siteName - STIG compliant - alternateHostName is $runtimeConfig.alternateHostName
+
+        }
+
+    }
+
+}
