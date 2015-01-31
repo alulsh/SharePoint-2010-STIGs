7df93677ddb53034077cc11cee6aca7285712fcb
Alexandra Ulsh <alexandra.ulsh@gmail.com>
2015-01-31 14:25:47 -0500

    Added 2 scripts to IIS 7 Site

---
+++ IIS Site/V-26044.ps1
@@ -0,0 +1,52 @@
+﻿<#
+Applies finding V-26044 from the IIS 7 Site STIG
+Title: The web-site must not allow non-ASCII characters in URLs.
+Reference: http://www.stigviewer.com/stig/iis_7.0_web_site/2014-03-25/finding/V-26044
+#>
+
+function applyHighBitCharacters {
+
+    $serverConfig = "/system.webServer/security/requestFiltering"
+
+    $requestFiltering = Get-WebConfiguration -Filter $serverConfig
+
+    if($requestFiltering.allowHighBitCharacters -eq $true){
+
+        Write-Host Server configuration is not STIG compliant - setting allow high bit characters to false
+
+        $requestFiltering.allowHighBitCharacters = $false
+        $requestFiltering | Set-WebConfiguration -Filter $serverConfig -PSPath IIS:\
+
+    }
+
+    else{
+
+        Write-Host Server configuration is STIG Compliant - allow high bit characters already set to false
+
+    }
+
+    $websites = Get-Website
+
+    foreach($website in $websites){
+
+        $siteName = $website.Name
+
+        $requestFiltering = Get-WebConfiguration -Filter $serverConfig -Location $siteName
+
+        if ($requestFiltering.allowHighBitCharacters -eq $true){
+
+            Write-Host $siteName is not STIG compliant - setting allow high bit characters to false
+
+            Set-WebConfigurationProperty -Filter $serverConfig -Name allowHighBitCharacters -Value False -PSPath IIS:\sites\$siteName
+
+        }
+
+        else {
+
+            Write-Host $siteName is STIG compliant - allow high bit chracters is already set to false
+
+        }
+
+    }
+
+}
---
+++ IIS Site/V-26045.ps1
@@ -0,0 +1,52 @@
+﻿<#
+Applies finding V-26045 from the IIS 7 Site STIG
+Title: The web-site must not allow double encoded URL requests.
+Reference: http://www.stigviewer.com/stig/iis_7.0_web_site/2014-03-25/finding/V-26045
+#>
+
+function applyDoubleEscapingURLs {
+
+    $serverConfig = "/system.webServer/security/requestFiltering"
+
+    $requestFiltering = Get-WebConfiguration -Filter $serverConfig
+
+    if($requestFiltering.allowDoubleEscaping -eq $true){
+
+        Write-Host Server configuration is not STIG compliant - setting double escaping to false
+
+        $requestFiltering.allowDoubleEscaping = $false
+        $requestFiltering | Set-WebConfiguration -Filter $serverConfig -PSPath IIS:\
+
+    }
+
+    else{
+
+        Write-Host Server configuration is STIG Compliant - allow double escaping already set to false
+
+    }
+
+    $websites = Get-Website
+
+    foreach($website in $websites){
+
+        $siteName = $website.Name
+
+        $requestFiltering = Get-WebConfiguration -Filter $serverConfig -Location $siteName
+
+        if ($requestFiltering.allowDoubleEscaping -eq $true){
+
+            Write-Host $siteName is not STIG compliant - setting allow double escaping to false
+
+            Set-WebConfigurationProperty -Filter $serverConfig -Name allowDoubleEscaping -Value False -PSPath IIS:\sites\$siteName
+
+        }
+
+        else {
+
+            Write-Host $siteName is STIG compliant - allow double escaping is already set to false
+
+        }
+
+    }
+
+}
